139c595c94ee2bdac5601da963f72f4cb75e8451
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-08 21:31:57 +0700

    hide navbar in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kin-mai-web';
+  isLogin = false;
+  public currentPath: string = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.currentPath = event.url;
+      });
+  }
+
+  ngOnInit() {
+    this.currentPath = this.router.url;
+  }
 }
