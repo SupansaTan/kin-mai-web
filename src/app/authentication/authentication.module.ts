f3a7af16038a723f5cf62002fd074da29e9a7685
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-12 07:12:48 +0700

    change file path

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
-import { RegisterRestuarantComponent } from './register-restuarant/register-restuarant.component';
-import { RegisterReviewerComponent } from './register-reviewer/register-reviewer.component';
+import { RegisterRestuarantComponent } from './register/register-restuarant/register-restuarant.component';
+import { RegisterReviewerComponent } from './register/register-reviewer/register-reviewer.component';
 import { RegisterComponent } from './register/register.component';
 import { ResetComponent } from './reset/reset.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
