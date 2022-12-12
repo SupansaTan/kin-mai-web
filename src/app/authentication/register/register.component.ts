59da4d48863cd3dee3e16f69b32d2ef81f659592
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-12 07:14:53 +0700

    change style register page

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountType } from 'src/enum/account-type.enum';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  isSelectedUserType: boolean = false;
+  userType: AccountType = AccountType.Reviewer;
+  accountType = AccountType;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setUserType(userType: AccountType) {
+    this.isSelectedUserType = true;
+    this.userType = userType;
+  }
 }
