3a7cab1604e1bb3fe31968f4cd5d65574c1d6ee1
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-15 01:55:47 +0700

    add field of social contact and business hour

--- src/app/authentication/register/register-restaurant/restaurant-info/restaurant-info.component.ts
+++ src/app/authentication/register/register-restaurant/restaurant-info/restaurant-info.component.ts
@@ -1,9 +1,10 @@
+import { SocialContactType } from './../../../../../constant/social-contact-type.constant';
 import { DeliveryType } from './../../../../../constant/delivery-type.constant';
 import { PaymentMethod } from './../../../../../constant/payment-method.constant';
 import { FoodCategory } from './../../../../../constant/food-category.constant';
 import { RestaurantType } from './../../../../../constant/restaurant-type.constant';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurant-info',
@@ -18,6 +19,7 @@ export class RestaurantInfoComponent implements OnInit {
   foodCategory = FoodCategory;
   paymentMethod = PaymentMethod;
   deliveryType = DeliveryType;
+  socialContactType = SocialContactType;
 
   constructor(private fb: FormBuilder) {
     this.registerRestaurantForm = this.fb.group({
@@ -42,14 +44,9 @@ export class RestaurantInfoComponent implements OnInit {
       foodCategory: new FormArray([]),
       deliveryType: new FormArray([]),
       paymentMethod: new FormArray([]),
-      socialContact: this.fb.array([
-        new FormGroup({
-          contact: new FormControl(null),
-          contactValue: new FormControl('')
-        })
-      ]),
+      socialContact: this.fb.array([]),
       businessHour: this.fb.array([
-        new FormGroup({
+        this.fb.group({
           day: new FormControl(null, [
             Validators.required
           ]),
@@ -64,17 +61,97 @@ export class RestaurantInfoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get FoodCategoryArray(): FormArray {
+    return this.registerRestaurantForm.get('foodCategory') as FormArray;
+  }
+
+  get PaymentMethodArray(): FormArray {
+    return this.registerRestaurantForm.get('paymentMethod') as FormArray;
+  }
+
+  get SocialContactArray(): FormArray {
+    return this.registerRestaurantForm.get('socialContact') as FormArray;
+  }
+
+  get BusinessHourArray(): FormArray {
+    return this.registerRestaurantForm.get('businessHour') as FormArray;
+  }
+
+  get DeliveryTypeArray(): FormArray {
+    return this.registerRestaurantForm.get('deliveryType') as FormArray;
+  }
+
+  getContactValueFormControl(index: number): AbstractControl {
+    const socialContact = this.SocialContactArray.controls[index] as FormGroup;
+    return socialContact.controls['contactValue'];
+  }
+
+  getDayFormControl(index: number): AbstractControl {
+    const businessHour = this.BusinessHourArray.controls[index] as FormGroup;
+    return businessHour.controls['day'];
+  }
+
+  getTimeFormControl(index: number): AbstractControl {
+    const businessHour = this.BusinessHourArray.controls[index] as FormGroup;
+    return businessHour.controls['time'];
+  }
+
+  get FoodCategoryList(): Array<string> {
+    const foodCategoryForm = this.registerRestaurantForm.get('foodCategory')?.value;
+    return Array.from(foodCategoryForm);
+  }
+
+  get DeliveryTypeList(): Array<string> {
+    const deliveryTypeForm = this.registerRestaurantForm.get('deliveryType')?.value;
+    return Array.from(deliveryTypeForm);
+  }
+
+  addSocialContact() {
+    this.SocialContactArray.markAllAsTouched();
+
+    if (this.SocialContactArray.valid) {
+      const newSocialContactForm = this.fb.group({
+        contact: new FormControl(null, [
+          Validators.required,
+        ]),
+        contactValue: new FormControl('', [
+          Validators.required,
+        ])
+      })
+      this.SocialContactArray.push(newSocialContactForm);
+    }
+  }
+
+  addBusinessHour() {
+    this.BusinessHourArray.markAllAsTouched();
+
+    if (this.BusinessHourArray.valid) {
+      const newBusinessHourForm = this.fb.group({
+        day: new FormControl(null, [
+          Validators.required
+        ]),
+        time: new FormControl('', [
+          Validators.required
+        ])
+      })
+      this.BusinessHourArray.push(newBusinessHourForm);
+    }
+  }
+
+  removeSocialContact(index: number) {
+    this.SocialContactArray.removeAt(index);
+  }
+
+  removeBusinessHour(index: number) {
+    this.BusinessHourArray.removeAt(index);
+  }
+
   checkFormIsValid() {
     this.registerRestaurantForm.markAllAsTouched();
 
     if (this.registerRestaurantForm.valid) {
       this.isFormValid.emit(true);
     }
-  }
-
-  get DeliveryTypeArray() {
-    console.log(this.registerRestaurantForm.get('deliveryType') as FormArray)
-    return this.registerRestaurantForm.get('deliveryType') as FormArray;
   }
 }
 
---
+++ src/constant/social-contact-type.constant.ts
@@ -0,0 +1,18 @@
+export const SocialContactType = [
+  {
+    id: 1,
+    name: 'เบอร์โทร'
+  },
+  {
+    id: 2,
+    name: 'Facebook'
+  },
+  {
+    id: 3,
+    name: 'Line'
+  },
+  {
+    id: 4,
+    name: 'Instagram'
+  },
+]
