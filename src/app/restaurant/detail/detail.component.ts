9b09c4bfa0a9e2c66e39a2559b867240b6f81964
hathaipat-p <hathaipat44@hotmail.co.th>
2022-12-22 21:17:10 +0700

    add res info

--- src/app/restaurant/detail/detail.component.ts
+++ src/app/restaurant/detail/detail.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+  Info: any = {
+    Rating: 3.5,
+    TotalReview: 51,
+    Menus: ["กระเพรา", "ข้าวผัด", "ก๋วยเตี๋ยว"],
+    Stars: [ "star", "star", "star", "star_half", "star_empty" ],
+    Payments: [ "เงินสด", "รับโอน" ],
+    Images: [ "../../../assets/image/image.svg", "../../../assets/image/image.svg", "../../../assets/image/image.svg" ],
+    Types: [ "อาหาร", "เครื่องดื่ม"],
+  }
+  
+  
 
   constructor() { }
 
