b8b4e6b93a36dadb5de0635e94969e5b91f7233c
hathaipat-p <hathaipat44@hotmail.co.th>
2022-12-27 02:01:55 +0700

    draft [reviewer] res-detail

--- src/app/reviewer/restaurant-detail/restaurant-detail.component.ts
+++ src/app/reviewer/restaurant-detail/restaurant-detail.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
+  Info: any = {
+    Rating: 3.5,
+    TotalReview: 51,
+    Menus: ["กระเพรา", "ข้าวผัด", "ก๋วยเตี๋ยว"],
+    Stars: [ "star", "star", "star", "star_half", "star_empty" ],
+    Payments: [ "เงินสด", "รับโอน" ],
+    Images: [ "../../../assets/image/image.svg", "../../../assets/image/image.svg", "../../../assets/image/image.svg" ],
+    Types: [ "อาหาร", "เครื่องดื่ม"],
+  }
+
+  Reviews: any = [ {}, {}, {}, ]
+
   constructor() { }
 
   ngOnInit(): void {
