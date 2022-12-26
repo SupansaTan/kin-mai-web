d28ddd2d996a236e26a45ac22951d4e55f129f9d
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-27 01:28:46 +0700

    add local storage service

---
+++ src/constant/local-storage-key.constant.ts
@@ -0,0 +1,9 @@
+export const LocalStorageKey = {
+  accessToken: 'accessToken',
+  refreshToken: 'refreshToken',
+  expirationToken: 'expirationToken',
+  userName: 'userName',
+  userType: 'userType',
+  userId: 'userId',
+  restaurantName: 'restaurantName'
+}
