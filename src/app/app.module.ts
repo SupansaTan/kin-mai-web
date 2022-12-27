ee5f05fb3d313b99090758dce04b56d9134db90c
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-27 12:55:01 +0700

    add progress bar under navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { MbscModule } from '@mobiscroll/angular';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { GuardsCheckEnd, NavigationEnd, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgProgressModule } from 'ngx-progressbar';
+import { NgProgressRouterModule } from 'ngx-progressbar/router';
+import { NgProgressHttpModule } from 'ngx-progressbar/http';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +32,16 @@ import { AuthenticationService } from './authentication/authentication.service';
     SharedModule,
     ModalModule.forRoot(),
     BrowserAnimationsModule,
+    NgProgressModule,
+    NgProgressHttpModule.withConfig({
+      id: 'progressBar'
+    }),
+    NgProgressRouterModule.withConfig({
+      startEvents: [GuardsCheckEnd],
+      completeEvents: [NavigationEnd],
+      delay: 1000,
+      id: 'progressBar'
+    })
   ],
   providers: [
     AuthenticationService,
