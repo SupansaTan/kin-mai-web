090f2fc793757b3203382060e10445a0468691ed
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-12-29 00:35:35 +0700

    route to register on login with google first time

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AccountType } from 'src/enum/account-type.enum';
 
 @Component({
@@ -6,14 +7,28 @@ import { AccountType } from 'src/enum/account-type.enum';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  private sub: any;
+
   isSelectedUserType: boolean = false;
   userType: AccountType = AccountType.Reviewer;
   accountType = AccountType;
+  firstName: string;
+  lastName: string;
+  email: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.firstName = params['firstName'];
+      this.lastName = params['lastName'];
+      this.email = params['email'];
+   });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   setUserType(userType: AccountType) {
--- src/app/service/google-auth.service.ts
+++ src/app/service/google-auth.service.ts
@@ -1,9 +1,14 @@
+import { UserInfoModel } from './../../models/user-info.model';
+import { ResponseModel } from './../../models/response.model';
+import { AuthenticationService } from './../authentication/authentication.service';
 import { PageLink } from './../../constant/path-link.constant';
 import { Injectable } from '@angular/core';
 import { SocialAuthService } from "@abacritt/angularx-social-login";
 import { SocialUser } from "@abacritt/angularx-social-login";
 import { GoogleLoginProvider } from "@abacritt/angularx-social-login";
 import { Router } from '@angular/router';
+import { LocalStorageService } from './local-storage.service';
+import { LocalStorageKey } from 'src/constant/local-storage-key.constant';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +20,8 @@ export class GoogleAuthService {
 
   constructor(
     private authService: SocialAuthService,
+    private authenticationService: AuthenticationService,
+    private localStorageService: LocalStorageService,
     private router: Router
     ) {
     this.authService.authState.subscribe((user) => {
@@ -25,7 +32,36 @@ export class GoogleAuthService {
 
   login(): void {
     const email = this.user.email;
-
+    this.authenticationService
+      .checkIsLoginWithGoogleFirstTimes(email)
+      .subscribe((response: ResponseModel<boolean>) => {
+        if (response.status === 200) {
+          if (response.data) {
+            // login first time --> go to register
+            this.router.navigate([PageLink.authentication.register, {
+              firstName: this.user.firstName,
+              lastName: this.user.lastName,
+              email: this.user.email
+            }]);
+          } else {
+            // already have account --> get user info
+            this.authenticationService
+              .getUserInfo(email)
+              .subscribe((resp: ResponseModel<UserInfoModel>) => {
+                if (resp.status === 200) {
+                  this.localStorageService.set(LocalStorageKey.userId, resp.data.userId);
+                  this.localStorageService.set(LocalStorageKey.userName, resp.data.userName);
+                  this.localStorageService.set(LocalStorageKey.restaurantName, resp.data.restaurantName);
+                  this.localStorageService.set(LocalStorageKey.userType, resp.data.userType);
+                  this.authenticationService.loginSuccessEvent(true);
+                  this.router.navigate([PageLink.reviewer.homepage]);
+                }
+              }
+            );
+          }
+        }
+      }
+    );
   }
 
   refreshToken(): void {
