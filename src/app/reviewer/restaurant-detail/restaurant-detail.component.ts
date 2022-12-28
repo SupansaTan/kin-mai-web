bfde1116e8dc6f6a515fddc9230a4ac21ccb6beb
hathaipat-p <hathaipat44@hotmail.co.th>
2022-12-29 00:40:49 +0700

    config layout [reviewer res-detail]

--- src/app/reviewer/restaurant-detail/restaurant-detail.component.ts
+++ src/app/reviewer/restaurant-detail/restaurant-detail.component.ts
@@ -10,10 +10,10 @@ export class RestaurantDetailComponent implements OnInit {
   Info: any = {
     Rating: 3.5,
     TotalReview: 51,
-    Menus: ["กระเพรา", "ข้าวผัด", "ก๋วยเตี๋ยว"],
+    Menus: ["กระเพรา", "ข้าวผัด", "ก๋วยเตี๋ยว","ราดหน้า", "คั่วกลิ้ง", "ผัดมาม่า"],
     Stars: [ "star", "star", "star", "star_half", "star_empty" ],
     Payments: [ "เงินสด", "รับโอน" ],
-    Images: [ "../../../assets/image/image.svg", "../../../assets/image/image.svg", "../../../assets/image/image.svg" ],
+    Images: [ "../../../assets/image/food-real.jpg", "../../../assets/image/food-real.jpg"],
     Types: [ "อาหาร", "เครื่องดื่ม"],
   }
 
