2da1e3fae78022d6ccbd2b7d398fffa6b568f780
hathaipat-p <hathaipat44@hotmail.co.th>
2023-01-14 16:05:55 +0700

    add edit-res-info component

---
+++ src/app/restaurant/edit-detail/edit-restaurant-info/edit-restaurant-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-restaurant-info',
+  templateUrl: './edit-restaurant-info.component.html',
+  styleUrls: ['./edit-restaurant-info.component.scss']
+})
+export class EditRestaurantInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/restaurant/restaurant.module.ts
+++ src/app/restaurant/restaurant.module.ts
@@ -7,12 +7,14 @@ import { RestaurantDashboardComponent } from './dashboard/dashboard.component';
 import { EditRestaurantDetailComponent } from './edit-detail/edit-detail.component';
 import { RestaurantRoutingModule } from './restaurant-routing.module';
 import { DetailComponent } from './detail/detail.component';
+import { EditRestaurantInfoComponent } from './edit-detail/edit-restaurant-info/edit-restaurant-info.component';
 
 @NgModule({
   declarations: [
     RestaurantDashboardComponent,
     EditRestaurantDetailComponent,
-    DetailComponent
+    DetailComponent,
+    EditRestaurantInfoComponent
   ],
   imports: [
     CommonModule,
