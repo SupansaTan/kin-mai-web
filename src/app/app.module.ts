998f4e205fe6ff25fd2e45cb929b5f44917c8cdf
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-01-18 01:30:09 +0700

    install ngx spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NgProgressHttpModule } from 'ngx-progressbar/http';
 import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -37,6 +38,7 @@ import { GoogleAuthService } from './service/google-auth.service';
     BrowserAnimationsModule,
     NgProgressModule,
     GoogleMapsModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-spin-clockwise' }),
     NgProgressHttpModule.withConfig({
       id: 'progressBar'
     }),
