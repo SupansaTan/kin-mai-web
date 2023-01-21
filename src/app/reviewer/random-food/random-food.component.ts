64a9318ffe2fd208bfe5942c1531a333f6f4118f
natthamon55 <natthamon200@gmail.com>
2023-01-22 02:18:09 +0700

    add random food game

--- src/app/reviewer/random-food/random-food.component.ts
+++ src/app/reviewer/random-food/random-food.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 
+
 @Component({
   selector: 'app-random-food',
   templateUrl: './random-food.component.html',
@@ -8,12 +9,18 @@ import Swal from 'sweetalert2';
 })
 export class RandomFoodComponent implements OnInit {
 
+  Foods=["อาหารไทย","อาหารนานาชาติ","อาหารตามสั่ง","อาหารฮาลาล","บุฟเฟ่ห์","สตรีทฟู๊ด/รถเข็น","อาหารอีสาน","ก๋วยเตี๋ยว","อาหารคลีน"];
+  randomWord: string;
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   ImageClick() {
-    Swal.fire('อาหารไทย');
+    this.randomWord = this.Foods[Math.floor(Math.random() * this.Foods.length)];
+    Swal.fire(this.randomWord);
   }
+
 }
