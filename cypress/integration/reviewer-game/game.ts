1018e75e41c38f234a7032641a2d7fa949bdae2e
hathaipat-p <hathaipat44@hotmail.co.th>
2023-02-06 00:27:02 +0700

    test random game

--- cypress/integration/reviewer-game/game.ts
+++ cypress/integration/reviewer-game/game.ts
@@ -4,36 +4,72 @@ Given(`I visit on random game page`, () => {
   cy.visit('/reviewer/random');
 });
 
-Then('I should see gachapon game ', () => {
+Then('I should see gachapon game', () => {
   cy.get('[data-cy="gachapon game"]').should('be.visible');
 });
 
-And('I should see button', (dataTable) => {
-  dataTable.hashes().forEach((item: { button: string }) => {
-    cy.get(`[data-cy="${item.button}"]`).should('be.visible');
-  });
+// --------------------------
+
+Given(`I visit on random game page`, () => {
+  cy.visit('/reviewer/random');
+});
+
+Then('I should see gachapon game ', () => {
+  cy.get('[data-cy="gachapon game"]').should('be.visible');
 });
 
 When('I click on gachapon game', () => {
   cy.get(`[data-cy="gachapon game"]`).click();
 });
 
-Then('I should recieve "food categories"',() =>{
-  cy.get('[data-cy="food categories"]');
+Then('I should see button', (dataTable) => {
+  dataTable.hashes().forEach((item: { button: string }) => {
+    cy.get(`[data-cy="${item.button}"]`).should('be.visible');
+  });
+});
+
+// ---------------------------
+
+Given(`I visit on random game page`, () => {
+  cy.visit('/reviewer/random');
+});
+
+When('I click on gachapon game', () => {
+  cy.get(`[data-cy="gachapon game"]`).click();
+});
+
+Then('I see button search restaurant', () => {
+  cy.wait(6000);
+  cy.get('[data-cy="SearchRestaurantBtn"]').should('be.visible');
 });
 
 When('I click on search restaurant', () => {
   cy.get(`[data-cy="SearchRestaurantBtn"]`).click();
 });
 
-Then('I should be on "search restaurant page"',() =>{
+Then('I should be on search restaurant page',() =>{
   cy.location('pathname', { timeout: 5000 }).should('eq', '/reviewer/search');
 });
 
-When('I click on restarant playlists', () => {
+// ---------------------------
+
+Given(`I visit on random game page`, () => {
+  cy.visit('/reviewer/random');
+});
+
+When('I click on gachapon game', () => {
+  cy.get(`[data-cy="gachapon game"]`).click();
+});
+
+Then('I see button search restarant playlists', () => {
+  cy.wait(6000);
+  cy.get('[data-cy="RestarantPlaylistsBtn"]').should('be.visible');
+});
+
+When('I click on search restarant playlists', () => {
   cy.get(`[data-cy="RestarantPlaylistsBtn"]`).click();
 });
 
-// Then('I should be on "restarant playlists page"',() =>{
-//   cy.visit('/reviewer/search');
-// });
+Then('I should be on search restaurant playlists page',() =>{
+  cy.location('pathname', { timeout: 5000 }).should('eq', '/reviewer/playlist');
+});
