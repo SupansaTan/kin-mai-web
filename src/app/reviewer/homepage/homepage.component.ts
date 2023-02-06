5b30e1631e6376a6ba874d6d908baa0ddae1bfa6
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-02-06 15:52:08 +0700

    integrate api get restaurant near me list

--- src/app/reviewer/homepage/homepage.component.ts
+++ src/app/reviewer/homepage/homepage.component.ts
@@ -1,6 +1,13 @@
+import { ResponseModel } from './../../../models/response.model';
+import { LocalStorageKey } from './../../../constant/local-storage-key.constant';
+import { LocalStorageService } from './../../service/local-storage.service';
+import { GetRestaurantNearMeRequestModel } from './../../../models/reviewer-homepage.model';
+import { ReviewerService } from './../reviewer.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { RestaurantInfoItemModel, RestaurantInfoListModel } from './../../../models/restaurant-info.model';
 import { ModalDessertComponent } from '../modal-dessert/modal-dessert.component';
 import { ModalFoodComponent } from '../modal-food/modal-food.component';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-homepage',
@@ -11,14 +18,65 @@ export class ReviewerHomepageComponent implements OnInit {
   @ViewChild('successModalFoodComponent') successModalFood: ModalFoodComponent;
   @ViewChild('successModalDessertComponent') successModalDessert: ModalDessertComponent;
 
-  currentRate = 4.5;
-  Info : any = {
-  Stars: [ "star", "star", "star", "star", "star_half"],
-  Images: [ "../../../assets/image/food1.jpg", "../../../assets/image/food2.jpg", "../../../assets/image/food3.jpg" ]
-  }
-  constructor() { }
+  restaurantInfoList: Array<RestaurantInfoItemModel>;
+  restaurantNearMeInfo: RestaurantInfoListModel;
+  awsS3Url = environment.awsS3Url;
+  totalRestaurant: number = 0;
+  restaurantCumulativeCount: number = 0;
+  isError: boolean;
+  isLoading: boolean = true;
+  skip: number = 0;
+  lat: number;
+  lng: number;
+
+  constructor(
+    private reviewerService: ReviewerService,
+    private localStorageService: LocalStorageService
+  ) { }
 
   ngOnInit(): void {
+    this.getUserCurrentLocation();
+  }
+
+  getUserCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.getRestaurantNearMeList();
+      },
+      (err) => {
+        // User not allowed to get current position
+        // set coordinates at Bangkok, Thailand
+        this.lat = 13.736717;
+        this.lng = 100.523186;
+        this.getRestaurantNearMeList();
+      },
+      {timeout:10000}
+    );
+  }
+
+  getRestaurantNearMeList() {
+    let request = new GetRestaurantNearMeRequestModel();
+    request.userId = this.localStorageService.get<string>(LocalStorageKey.userId) ?? '';
+    request.latitude = this.lat;
+    request.longitude = this.lng;
+    request.skip = this.skip;
+    request.take = 10;
+
+    this.reviewerService.getRestaurantNearMeList(request)
+      .subscribe((response: ResponseModel<RestaurantInfoListModel>) => {
+      if (response?.status === 200) {
+        this.restaurantNearMeInfo = response.data;
+        this.restaurantInfoList = response.data.restaurantInfo;
+        this.totalRestaurant = response.data.totalRestaurant;
+        this.restaurantCumulativeCount = response.data.restaurantCumulativeCount;
+        this.isLoading = false;
+      } else {
+        this.isError = true;
+        this.isLoading = false;
+      }
+    })
   }
 
   openModalFood() {
@@ -27,5 +85,9 @@ export class ReviewerHomepageComponent implements OnInit {
 
   openModalDessert() {
     this.successModalDessert.openSuccessModal();
+  }
+
+  toggleFavoriteRestaurant(restaurantId: string, isFavorite: boolean) {
+
   }
 }
