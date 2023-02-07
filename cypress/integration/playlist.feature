0d86a366aae44d506b83f9ba7a3abf3965d9e307
natthamon55 <natthamon200@gmail.com>
2023-02-07 11:07:04 +0700

    add playlist test and del some test

--- cypress/integration/playlist.feature
+++ cypress/integration/playlist.feature
@@ -1,7 +1,22 @@
 Feature: Reviewer search playlist
   Reviewer can search playlist
 
-  Scenario: Reviewer search somtam playlist
+  Background:
     Given I visit on the playlist page
     Then  I should see restaurant playlist
+
+  Scenario: Reviewer search for somtam restaurant
+    When  I search "somtam" in a search box
+      | searchBox |
+      | ส้มตำ |
+    Then  I should see list of "somtam restaurant" playlist
+
+  Scenario: Reviewer click playlist title
+    When  I click on title of detail
+    Then  I should be on playlist-detail page
+  
+  
+
+  
+
   
--- cypress/integration/playlist/playlist.ts
+++ cypress/integration/playlist/playlist.ts
@@ -1,10 +1,29 @@
-import { Given,Then } from "cypress-cucumber-preprocessor/steps";
+import { Given,Then,When} from "cypress-cucumber-preprocessor/steps";
 
 Given(`I visit on the playlist page`, () => {
   cy.visit('reviewer/playlist');
 })
 
 Then('I should see restaurant playlist', () => {
+  cy.wait(2000);
   cy.get('[data-cy="RestaurantPlaylist"]').should('be.visible');
 });
 
+When ('I search "somtam" in a search box', (dataTable) => {
+  dataTable.hashes().forEach((item: { searchBox : string }) => {
+    cy.get(`[data-cy="searchBox"]`).type(item.searchBox, {force: true});
+  });
+});
+
+Then('I should see list of "somtam restaurant" playlist', () => {
+  cy.get('[data-cy="RestaurantPlaylist"]').should('be.visible');
+});
+
+When('I click on title of detail', () => {
+  cy.get('[data-cy="PlaylistDetail"]').first().click();
+});
+
+Then ('I should be on playlist-detail page',() =>{
+  cy.wait(2000);
+  cy.location('pathname', { timeout: 5000 }).should('eq', '/reviewer/playlist-detail');
+});
