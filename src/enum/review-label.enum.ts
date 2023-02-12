71c289bf01e905a1ef4a07742007f5011bfe2433
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-02-13 03:13:54 +0700

    implement recommend review label in modal review

---
+++ src/enum/review-label.enum.ts
@@ -0,0 +1,15 @@
+export enum BadReviewLabel {
+  BadPackage = 1,
+  IncorrectOrder = 2,
+  PoorQuality = 3,
+  DoesNotLookLikePicture = 4,
+  UnreasonablePrice = 5,
+}
+
+export enum GoodReviewLabel {
+  CorrectOrder = 6,
+  GoodPackage = 7,
+  Cleanliness = 8,
+  Delicious = 9,
+  GreatValue = 10,
+}
