636afd90ee2694ba073d5ab5ec49eb43fdf4384b
hathaipat-p <hathaipat44@hotmail.co.th>
2023-02-19 01:15:56 +0700

    add service get restaurant detail
    [in progress]

--- src/app/restaurant/detail/detail.component.ts
+++ src/app/restaurant/detail/detail.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ModalGalleryComponent } from '../modal-gallery/modal-gallery.component';
+import { GetReviewInfoRequest, ReviewInfoModel } from 'src/models/review-info.model';
+import { RestaurantService } from '../restaurant.service';
+import { LocalStorageService } from 'src/app/service/local-storage.service';
+import { LocalStorageKey } from 'src/constant/local-storage-key.constant';
+import { ResponseModel } from 'src/models/response.model';
+import { Restaurant, RestaurantDetailModel, SocialContactModel } from 'src/models/restaurant-info.model';
+import { RestaurantType } from 'src/constant/restaurant-type.constant';
+import { PaymentMethod } from 'src/constant/payment-method.constant';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-detail',
@@ -9,27 +18,125 @@ import { ModalGalleryComponent } from '../modal-gallery/modal-gallery.component'
 export class DetailComponent implements OnInit {
 
   @ViewChild('modalGalleryComponent') modalGallery: ModalGalleryComponent;
+  @Input() isLoading: boolean = true;
 
-  Info: any = {
-    Rating: 3.5,
-    TotalReview: 51,
-    Menus: ["กระเพรา", "ข้าวผัด", "ก๋วยเตี๋ยว","ราดหน้า", "มาม่าผัด", "เขียวหวาน"],
-    Stars: [ "star", "star", "star", "star_half", "star_empty" ],
-    Payments: [ "เงินสด", "รับโอน" ],
-    Images: [ "../../../assets/image/noodle.jpg", "../../../assets/image/food1.jpg", "../../../assets/image/food2.jpg"
-              , "../../../assets/image/food3.jpg", "../../../assets/image/icecream.jpg", "../../../assets/image/juice.jpg"
-              , "../../../assets/image/dessert.jpg", "../../../assets/image/cafe.jpg", "../../../assets/image/interfood.jpg"],
-    Types: [ "อาหาร", "เครื่องดื่ม"],
-  }
+  Info: Restaurant;
+  Reviews: Array<ReviewInfoModel>
+  SocialContact: Array<SocialContactModel>;
+  TotalReview: number;
+  Rating: number;
+  Star: Array<string>;
+  RestaurantType: Array<string>;
+  awsS3Url = environment.awsS3Url;
+  lat: number;
+  lng: number;
+  userId: string;
+  restaurantId: string;
 
-  constructor() { }
+  constructor(
+    private restaurantService: RestaurantService,
+    private localStorageService: LocalStorageService,
+  ) { }
 
   ngOnInit(): void {
+    this.userId = this.localStorageService.get<string>(LocalStorageKey.userId) ?? '';
+    this.restaurantId = this.localStorageService.get<string>(LocalStorageKey.restaurantId) ?? '';
+    this.getRestaurantDetail();
   }
 
   openModalGallery() {
     this.modalGallery.openSuccessModal();
   }
 
+  getRestaurantDetail() {
+    let request = new GetReviewInfoRequest();
+    request.userId = this.userId;
+    request.restaurantId = this.restaurantId;
+    this.restaurantService.getRestaurantDetail(request).subscribe(
+      (response: ResponseModel<RestaurantDetailModel>) => {
+        if (response && response?.status === 200) {
+          this.Info = response.data.restaurantInfo;
+          this.Reviews = response.data.reviews;
+          this.SocialContact = response.data.socialContact;
+          this.TotalReview = this.Reviews.length
+          let ratingCount = 0;
+          this.Reviews.forEach(x => { 
+            ratingCount += x.rating
+          });
+          this.Rating = ratingCount/this.Reviews.length
+          this.getStarArray()
+          this.isLoading = false;
+        }
+    })
+  }
+
+  getStarArray() {
+    switch (this.Rating) {
+      case 5:
+        this.Star = ["star", "star", "star", "star", "star"]
+        break;
+      case 4.5:
+        this.Star = ["star", "star", "star", "star", "star_half"]
+        break;
+      case 4:
+        this.Star = ["star", "star", "star", "star", "star_empty"]
+        break;
+      case 3.5:
+        this.Star = ["star", "star", "star", "star_half", "star_empty"]
+        break;
+      case 3:
+        this.Star = ["star", "star", "star", "star_empty", "star_empty"]
+        break;
+      case 2.5:
+        this.Star = ["star", "star", "star_half", "star_empty", "star_empty"]
+        break;
+      case 2:
+        this.Star = ["star", "star", "star_empty", "star_empty", "star_empty"]
+        break;
+      case 1:
+        this.Star = ["star", "star_empty", "star_empty", "star_empty", "star_empty"]
+        break;
+      case 0:
+        this.Star = ["star_empty", "star_empty", "star_empty", "star_empty", "star_empty"]
+        break;
+    }
+  }
+
+  getRestaurantType(type: number) {
+    switch (type) {
+      case 1:
+        return [RestaurantType.find((i:any) => i.id === 1)?.name]
+      case 2:
+        return [RestaurantType.find((i:any) => i.id === 1)?.name]
+      default:
+        return [RestaurantType.find((i:any) => i.id === 1)?.name , RestaurantType.find((i:any) => i.id === 2)?.name]
+    }
+  }
+
+  getPaymentMethod(method: number) {
+    switch (method) {
+      case 1:
+        return PaymentMethod.find((i:any) => i.id === 1)?.name;
+      case 2:
+        return PaymentMethod.find((i:any) => i.id === 2)?.name;
+      case 3:
+        return PaymentMethod.find((i:any) => i.id === 3)?.name;
+      default:
+        return PaymentMethod.find((i:any) => i.id === 4)?.name;
+    }
+  }
+
+  getSocialContactValue(type: number) {
+    switch (type) {
+      case 1:
+        return this.SocialContact.find((i:SocialContactModel) => i.socialType === 1)?.contactValue;
+      case 2:
+        return this.SocialContact.find((i:any) => i.socialType === 2)?.contactValue;
+      case 3: 
+        return this.SocialContact.find((i:any) => i.socialType === 3)?.contactValue;
+      default:
+        return this.SocialContact.find((i:any) => i.socialType === 4)?.contactValue;
+    }
+  }
 
 }
--- src/app/restaurant/restaurant.service.ts
+++ src/app/restaurant/restaurant.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { GetReviewInfoRequest } from 'src/models/review-info.model';
+import { environment } from 'src/environments/environment';
+import { ResponseModel } from 'src/models/response.model';
+import { RestaurantDetailModel } from 'src/models/restaurant-info.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
+  sub: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRestaurantDetail(model: GetReviewInfoRequest) {
+    const url = `${environment.kinMaiApi}/Restaurant/GetRestaurantDetail?userId=${model.userId}&restaurantId=${model.restaurantId}`;
+    // const url = `${environment.kinMaiApi}/Restaurant/GetRestaurantDetail?userId=${"3c52c576-f9af-41e8-8220-f269e1a196bd"}&restaurantId=${"5bba1de4-a671-441e-a24b-6220ee63936f"}`;
+    this.sub = this.http.get<ResponseModel<RestaurantDetailModel>>(url);
+    return this.sub;
+  }
+  
 }
