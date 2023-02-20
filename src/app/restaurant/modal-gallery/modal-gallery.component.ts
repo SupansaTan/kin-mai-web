0e0a716fc1933b08280024f227611aeacb547f67
hathaipat-p <hathaipat44@hotmail.co.th>
2023-02-20 22:26:07 +0700

    layout res-detail & link image to gallery

--- src/app/restaurant/modal-gallery/modal-gallery.component.ts
+++ src/app/restaurant/modal-gallery/modal-gallery.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal'
+import { environment } from 'src/environments/environment';
 
 // import Swiper core and required modules
 import SwiperCore, { FreeMode, Navigation, Thumbs } from "swiper";
@@ -17,16 +18,11 @@ export class ModalGalleryComponent {
 
   @ViewChild('modalGallery') modalGallery: TemplateRef<any>;
   @Output() closeModalEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() ImageData: Array<string>;
 
   modalRef: BsModalRef;
-
   thumbsSwiper: any;
-
-  Info: any = {
-    Images: [ "../../../assets/image/noodle.jpg", "../../../assets/image/food1.jpg", "../../../assets/image/food2.jpg"
-                , "../../../assets/image/food3.jpg", "../../../assets/image/icecream.jpg", "../../../assets/image/juice.jpg"
-                , "../../../assets/image/dessert.jpg", "../../../assets/image/cafe.jpg", "../../../assets/image/interfood.jpg"]
-  }
+  awsS3Url = environment.awsS3Url;
 
   constructor(
     private modalService: BsModalService,
