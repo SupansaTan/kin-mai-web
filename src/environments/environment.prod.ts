ab6d8551a07e17e38f1620b40c96ea5342fc4aa5
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-02-28 03:11:52 +0700

    add prod env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  kinMaiApi: process.env.kinMaiApi,
+  googleMapsApi: process.env.googleMapsApi,
+  googleClientId: process.env.googleClientId,
+  googleClientSecret: process.env.googleClientSecret,
+  awsS3Url: process.env.awsS3Url,
+};
