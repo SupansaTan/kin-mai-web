9a48e9467a0c8c27e503dda1443b9923720b147b
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-02-28 00:21:59 +0700

    add style to forgot password page

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -1,4 +1,9 @@
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/service/local-storage.service';
+import { AuthenticationService } from '../authentication.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-forget-password',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm: FormGroup;
+  isSentEmail: boolean = false;
 
-  constructor() { }
+  constructor(
+      private fb: FormBuilder
+    , private authenticationService: AuthenticationService
+    , private spinner: NgxSpinnerService
+    ) {
+    this.forgotPasswordForm = this.fb.group({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required
+      ]),
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.forgotPasswordForm.controls;
+  }
+
+  submit() {
+    this.forgotPasswordForm.markAllAsTouched();
+
+    if (this.forgotPasswordForm.valid) {
+      const email = this.forgotPasswordForm.get('username')?.value;
+      this.spinner.show();
+    }
+  }
 }
