ddc95391cf4b1f3362114a9ee90a91e45776e536
Supansa <supansa.tantulset@gmail.com>
2023-03-07 14:00:12 +0700

    add service send email forgot password

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -1,9 +1,12 @@
+import { LocalStorageKey } from 'src/constant/local-storage-key.constant';
+import { ResponseModel } from './../../../models/response.model';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/service/local-storage.service';
 import { AuthenticationService } from '../authentication.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ModalSuccessComponent } from 'src/app/shared/modal-success/modal-success.component';
 
 @Component({
   selector: 'app-forget-password',
@@ -11,6 +14,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  @ViewChild('successModal') successModal: ModalSuccessComponent;
+
   forgotPasswordForm: FormGroup;
   isSentEmail: boolean = false;
 
@@ -18,6 +23,7 @@ export class ForgotPasswordComponent implements OnInit {
       private fb: FormBuilder
     , private authenticationService: AuthenticationService
     , private spinner: NgxSpinnerService
+    , private localStorageService: LocalStorageService
     ) {
     this.forgotPasswordForm = this.fb.group({
       email: new FormControl('', [
@@ -38,8 +44,21 @@ export class ForgotPasswordComponent implements OnInit {
     this.forgotPasswordForm.markAllAsTouched();
 
     if (this.forgotPasswordForm.valid) {
-      const email = this.forgotPasswordForm.get('username')?.value;
       this.spinner.show();
+      const email = this.forgotPasswordForm.get('email')?.value;
+
+      this.authenticationService.sendEmailForgotPassword(email).subscribe(
+        (response: ResponseModel<boolean>) => {
+          this.spinner.hide();
+
+          if (response && response?.status === 200) {
+            this.isSentEmail = true;
+            this.forgotPasswordForm.reset();
+          } else {
+            this.isSentEmail = false;
+            this.successModal.openSuccessModal(false, response.message);
+          }
+      })
     }
   }
 }
